6efea1bcb0d0754b1678cc85d91ef3dda9e81470
Peter Stibrany <pstibrany@gmail.com>
2014-02-19 10:59:45 +0100

    Removed IfToken part. It wasn't used.

--- src/com/floreysoft/jmte/token/ElseToken.java
+++ src/com/floreysoft/jmte/token/ElseToken.java
@@ -6,33 +6,17 @@ import com.floreysoft.jmte.TemplateContext;
 public class ElseToken extends AbstractToken {
 	public static final String ELSE = "else";
 
-	protected IfToken ifToken = null;
-
 	@Override
 	public String getText() {
 		if (text == null) {
-			text = ELSE + getIfToken() != null ? "(" + getIfToken().getText()
-					+ ")" : "";
+			text = ELSE;
 		}
 		return text;
 	}
 
 	@Override
 	public Object evaluate(TemplateContext context) {
-		Boolean evaluated = !(Boolean) getIfToken().evaluate(context);
-		return evaluated;
-	}
-
-	public void setIfToken(IfToken ifToken) {
-		this.ifToken = ifToken;
-	}
-
-	public IfToken getIfToken() {
-		if (ifToken == null) {
-			throw new IllegalStateException(
-					"An else token can only be evaluated using an associated if token - which is missing");
-		}
-		return ifToken;
+        return "";
 	}
 
 	@Override
